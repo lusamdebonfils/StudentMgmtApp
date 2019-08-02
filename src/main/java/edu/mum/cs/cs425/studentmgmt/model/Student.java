b07fe8188de48963d06620e455e868df4a33748d
lusamdebonfils <samuelluswata@gmail.com>
2019-08-01 20:07:54 -0500

    Refactored the POJOs and created functionality, App ready for launch

--- src/main/java/edu/mum/cs/cs425/studentmgmt/model/Student.java
+++ src/main/java/edu/mum/cs/cs425/studentmgmt/model/Student.java
@@ -2,10 +2,14 @@ package edu.mum.cs.cs425.studentmgmt.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.lang.NonNull;
@@ -26,8 +30,19 @@ public class Student {
 	private String lastName;
 	private Double cgpa;
 	private LocalDate dateOfEnrollment;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	private Transcript transcript = new Transcript();
+	
+	
+	@ManyToOne
+	//@JoinColumn(name = "classroom_fk")
+	private Classroom classRoom = new Classroom();
+	
 	public Student() {
+		
 		super();
+		
 		// TODO Auto-generated constructor stub
 	}
 	public Student(long studentNumber, String firstName, String middleName, String lastName, Double cgpa,
@@ -39,6 +54,9 @@ public class Student {
 		this.lastName = lastName;
 		this.cgpa = cgpa;
 		this.dateOfEnrollment = dateOfEnrollment;
+//		this.transcript = new Transcript();
+//		this.classRoom = new Classroom();
+		
 	}
 	public Student(long studentNumber, String firstName, String lastName, LocalDate dateOfEnrollment) {
 		super();
@@ -53,6 +71,55 @@ public class Student {
 				+ ", middleName=" + middleName + ", lastName=" + lastName + ", cgpa=" + cgpa + ", dateOfEnrollment="
 				+ dateOfEnrollment + "]";
 	}
+	public long getStudentNumber() {
+		return studentNumber;
+	}
+	public void setStudentNumber(long studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Double getCgpa() {
+		return cgpa;
+	}
+	public void setCgpa(Double cgpa) {
+		this.cgpa = cgpa;
+	}
+	public LocalDate getDateOfEnrollment() {
+		return dateOfEnrollment;
+	}
+	public void setDateOfEnrollment(LocalDate dateOfEnrollment) {
+		this.dateOfEnrollment = dateOfEnrollment;
+	}
+	public Transcript getTranscript() {
+		return transcript;
+	}
+	public void setTranscript(Transcript transcript) {
+		this.transcript = transcript;
+	}
+	public Classroom getClassRoom() {
+		return classRoom;
+	}
+	public void setClassRoom(Classroom classRoom) {
+		this.classRoom = classRoom;
+	}
+	
 	
 	
 	
